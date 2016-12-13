42e5e1e7d901ee086a5e635275a5402e2c2aa60f
TheCoub <ryan.courbron@baxter-academy.org>
2016-12-13 14:55:37 -0500

    Added player movement

---
+++ Assets/note.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class note : MonoBehaviour {
+
+	//Create an initilize function that will set all the proper variable on awake. 
+
+	public string message = " ";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
---
+++ Assets/page.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class page : MonoBehaviour {
+
+	public Rigidbody2D phys;
+	public float moveSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		phys = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movement ();
+	}
+
+	void movement(){
+		if (Input.GetKey (KeyCode.W)) {
+			phys.velocity = Vector2.up * moveSpeed;
+		} else if (Input.GetKey (KeyCode.S)) {
+			phys.velocity = Vector2.up * moveSpeed * -1;
+		} else if (Input.GetKey (KeyCode.A)) {
+			phys.velocity = Vector2.right * moveSpeed * -1;
+		} else if (Input.GetKey (KeyCode.D)) {
+			phys.velocity = Vector2.right * moveSpeed;
+		} else {
+			phys.velocity = Vector2.zero;
+		}
+	}
+
+	void pickUp(){
+		if (Input.GetKey (KeyCode.Space)) {
+			
+		}
+	}
+}
